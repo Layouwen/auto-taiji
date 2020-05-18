7349bda65400999a8dbea6854c0c3c3c3b20d787
Layouwen <Layouwen@gmail.com>
2020-05-18 19:43:48 +0800

    finish js

---
+++ js/index.js
@@ -0,0 +1,85 @@
+let js_content = document.querySelector('.content')
+let js_style = document.querySelector('.js_style')
+
+let str = `
+/* 你好，我是一名前端工程师，因这学期课程接触到了前端
+
+学校教的很基础，主要靠自学，目前已经学习将近大半年
+
+接触前端后发现自己深深的爱上了编程，为了实现自己的一个界面或功能
+
+促使我发现能力和解决问题的能力
+
+下面展示我的一个小想法，实现一个PC/PE端自适应的太极 */
+
+.bigCircle {
+    position: relative;
+    width: 400px;
+    height: 400px;
+    border-radius: 50%;
+    background: linear-gradient(to right, #ffffff 0%, #ffffff 50%, #000000 50%, #000000 100%);
+    animation: turn 2s linear infinite;
+}
+
+.bigCircle::before,
+.bigCircle::after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    transform: translateX(-50%);
+}
+
+.bigCircle::before {
+    top: 0;
+    background: radial-gradient(ellipse at center, #000000 0%, #000000 25%, #ffffff 25%, #ffffff 100%);
+}
+
+.bigCircle::after {
+    bottom: 0;
+    background: radial-gradient(ellipse at center, #ffffff 0%, #ffffff 25%, #000000 25%, #000000 100%);
+}
+
+@keyframes turn {
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+/* 希望这个想法对刚学习动画和CSS的你有所帮助 */
+
+`
+
+let contentStr = ''
+let n = 0
+let speed = 0
+
+let run = () => {
+    setTimeout(() => {
+        if (str[n] === '\n') {
+            contentStr += '<br>'
+        } else if (str[n] === ' ') {
+            contentStr += '&nbsp;'
+        } else {
+            contentStr += str[n]
+        }
+        js_style.innerHTML = str.substring(0, n)
+        js_content.innerHTML = contentStr
+        n++;
+        window.scrollTo(0, 99999);
+        js_content.scrollTo(0, 99999)
+        if (n < str.length) {
+            run()
+        }
+    }, speed)
+}
+
+run()
+
+let js_slow = document.querySelector('.slow')
+
+js_slow.addEventListener('click', () => {
+    console.log('hi')
+})
