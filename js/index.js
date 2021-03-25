d03c2f341dd4c635f2b75b06cb6cf7743f53bf0f
YouwenLiang <layouwen@gmail.com>
2021-03-25 19:01:29 +0800

    refactor code

--- js/index.js
+++ js/index.js
@@ -14,46 +14,46 @@ let str = `
 下面展示我的一个小想法，实现一个PC/PE端自适应的太极 */
 
 @keyframes turn {
-    0%{
-        transform: rotate(0deg);
-    }
+  0%{
+    transform: rotate(0deg);
+  }
     100% {
-        transform: rotate(360deg);
-    }
+    transform: rotate(360deg);
+  }
 }
 
 .bigCircle {
-    position: relative;
-    width: 400px;
-    height: 400px;
-    border-radius: 50%;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-    background: linear-gradient(to right, #ffffff 0%, #ffffff 50%, #000000 50%, #000000 100%);
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border-radius: 50%;
+  box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+  background: linear-gradient(to right, #ffffff 0%, #ffffff 50%, #000000 50%, #000000 100%);
 }
 
 .bigCircle::before,
 .bigCircle::after {
-    content: '';
-    position: absolute;
-    left: 50%;
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    transform: translateX(-50%);
+  content: '';
+  position: absolute;
+  left: 50%;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  transform: translateX(-50%);
 }
 
 .bigCircle::before {
-    top: 0;
-    background: radial-gradient(ellipse at center, #000000 0%, #000000 25%, #ffffff 25%, #ffffff 100%);
+  top: 0;
+  background: radial-gradient(ellipse at center, #000000 0%, #000000 25%, #ffffff 25%, #ffffff 100%);
 }
 
 .bigCircle::after {
-    bottom: 0;
-    background: radial-gradient(ellipse at center, #ffffff 0%, #ffffff 25%, #000000 25%, #000000 100%);
+  bottom: 0;
+  background: radial-gradient(ellipse at center, #ffffff 0%, #ffffff 25%, #000000 25%, #000000 100%);
 }
 
 .bigCircle {
-    animation: turn 2s linear infinite;
+  animation: turn 2s linear infinite;
 }
 
 /* 希望这个想法对刚学习动画和CSS的你有所帮助 */
@@ -65,60 +65,52 @@ let speed = 50
 let time
 
 let run = () => {
-    time = setTimeout(() => {
-        if (str[n] === '\n') {
-            contentStr += '<br>'
-        } else if (str[n] === ' ') {
-            contentStr += '&nbsp;'
-        } else {
-            contentStr += str[n]
-        }
-        js_style.innerHTML = str.substring(0, n)
-        js_content.innerHTML = contentStr
-        n++;
-        js_content.scrollTo(0, 99999)
-        if (n < str.length) {
-            run()
-        } else {
-            n = 0
-            contentStr = ''
-            js_start.innerHTML = '开始'
-            js_start.className = 'start'
-        }
-    }, speed)
+  time = setTimeout(() => {
+    if (str[n] === '\n') {
+      contentStr += '<br>'
+    } else if (str[n] === ' ') {
+      contentStr += '&nbsp;'
+    } else {
+      contentStr += str[n]
+    }
+    js_style.innerHTML = str.substring(0, n)
+    js_content.innerHTML = contentStr
+    n++;
+    js_content.scrollTo(0, 99999)
+    if (n < str.length) run()
+    else {
+      n = 0
+      contentStr = ''
+      js_start.innerHTML = '开始'
+      js_start.id = ''
+      js_start.style = 'color: #000'
+    }
+  }, speed)
 }
 
-let js_slow = document.querySelector('.slow')
-let js_middling = document.querySelector('.middling')
-let js_quick = document.querySelector('.quick')
-let js_start = document.querySelector('#start')
+let js_start = document.querySelector('.start')
 
-js_slow.addEventListener('click', () => {
-    if (speed !== 100) {
-        speed = 200
-    }
-})
+const begin = dom => {
+  const text = dom.innerText
+  const isBegin = text === '开始'
+  isBegin ? run() : clearTimeout(time)
+  dom.innerHTML = isBegin ? '暂停' : '开始'
+  dom.id = isBegin ? 'active' : ''
+  dom.style = isBegin ? 'color: #fff' : 'color: #000'
+}
 
-js_middling.addEventListener('click', () => {
-    if (speed !== 50) {
-        speed = 50
-    }
-})
+const clickMap = {
+  'slow': 200,
+  'middling': 50,
+  'quick': 0,
+  'start': begin
+}
 
-js_quick.addEventListener('click', () => {
-    if (speed !== 0) {
-        speed = 0
-    }
-})
+const clickNames = Object.keys(clickMap)
 
-js_start.addEventListener('click', () => {
-    if (js_start.innerHTML === '开始') {
-        run()
-        js_start.innerHTML = '暂停'
-        js_start.className = ''
-    } else {
-        js_start.innerHTML = '开始'
-        clearTimeout(time)
-        js_start.className = 'start'
-    }
+document.querySelector('.buttons').addEventListener('click', (e) => {
+  const elClassName = e.target.className
+  if (!clickNames.includes(elClassName)) return (console.error("你的标签触发在，意料之外"))
+  if (elClassName === 'start') return begin(e.target)
+  speed = clickMap[elClassName]
 })
